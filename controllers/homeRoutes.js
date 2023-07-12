85e37508b9059a2bcbe0c03e28e5b4286f021f5a
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-07-11 23:19:54 -0700

    Added default user images to test out look of site & Setup pagination to go through pages of 16 posts on the home page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 
 router.get('^/$|/index(.html)?', async (req, res) => {
+  const page = req.query.page || 1;
   const postsData = await Post.findAll({
     include: [
       { model: User, attributes: { exclude: ['password'] } },
@@ -10,9 +11,33 @@ router.get('^/$|/index(.html)?', async (req, res) => {
         include: { model: User, attributes: { exclude: ['password'] } },
       },
     ],
+    limit: 16,
+    order: [['createdAt', 'DESC']],
+    offset: (page - 1) * 16,
   });
+  const postsCount = await Post.count();
+
+  const pages = Math.ceil(postsCount / 16);
+
+  const pagination = [];
+
+  for (let i = 0; i < pages; i++) {
+    if (i === pages - 1) {
+      pagination.push({ page: '...', active: false, disabled: true });
+    }
+    pagination.push({ page: i + 1, active: i + 1 === parseInt(page) });
+  }
+
+
   const posts = postsData.map((post) => post.get({ plain: true }));
-  res.render('home', { posts });
+
+  res.render('home', {
+    posts,
+    page,
+    pagination,
+    lastPage: pages === parseInt(page),
+    firstPage: parseInt(page) === 1,
+  });
 });
 
 router.get('*', (req, res) => {
---
+++ public/js/postLikes.js
@@ -0,0 +1,36 @@
+async function likePost(id) {
+  const btn = document.querySelectorAll(`button[data-post-id="${id}"]`)[0];
+  const small = btn.querySelector('small');
+  const likes = parseInt(btn.dataset.postLikes);
+  const data = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ likes: likes + 1 }),
+  });
+  if (data.ok) {
+    btn.dataset.postLikes = likes + 1;
+    small.textContent = likes + 1;
+  } else {
+    alert('Something went wrong');
+  }
+}
+async function dislikePost(id) {
+  const btn = document.querySelectorAll(`button[data-post-id="${id}"]`)[1];
+  const small = btn.querySelector('small');
+  const dislikes = parseInt(btn.dataset.postDislikes);
+  const data = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ dislikes: dislikes + 1 }),
+  });
+  if (data.ok) {
+    btn.dataset.postDislikes = dislikes + 1;
+    small.textContent = dislikes + 1;
+  } else {
+    alert('Something went wrong');
+  }
+}
