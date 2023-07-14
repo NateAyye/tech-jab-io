d48aa4a7431eed753329d6bfd20faee11c6a7195
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-07-14 16:13:45 -0700

    Fixed some errors that occured in the posts and dashboard rendering routes

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -8,9 +8,10 @@ router.use(async (req, res, next) => {
   const user = (await api.getUser(req.session.user.id)) || [];
   const userPosts = user.get({ plain: true }).posts;
 
-  const userLikes = user.posts
-    ?.map((post) => post?.dataValues?.likes)
+  const userLikes = userPosts
+    ?.map((post) => post?.likes)
     ?.reduce((a, b) => a + b, 0);
+  
   res.locals.userLikes = userLikes;
   res.locals.userPosts = userPosts;
   next();
@@ -20,8 +21,19 @@ router.get('/new-post', (req, res) => {
   res.render('dashboard/new-post', { layout: 'dashboard' });
 });
 
-router.get('/', (req, res) => {
-  res.render('dashboard', { layout: 'dashboard' });
+router.get('/edit-post/:id', async (req, res) => {
+  const id = req.params.id;
+  const postData = await api.getPost(id);
+  if (!postData) return res.status(500).json({ message: 'Failed to get post' });
+  const post = postData.get({ plain: true });
+  res.render('dashboard/edit-post', { post, layout: 'dashboard' });
+});
+
+router.get('/', async (req, res) => {
+  const userData = (await api.getUser(req.session.user.id)) || {};
+  const user = userData?.get({ plain: true });
+  const posts = user?.posts || [];
+  res.render('dashboard', { posts, isAuthor: true, layout: 'dashboard' });
 });
 
 module.exports = router;
--- controllers/posts.js
+++ controllers/posts.js
@@ -8,8 +8,7 @@ router.get('/:id', async (req, res) => {
   const postData = await api.getPost(id);
   if (!postData) return res.status(500).json({ message: 'Failed to get post' });
   const post = postData.get({ plain: true });
-
-  const isAuthor = post?.user_id === req.session.user.id;
+  const isAuthor = post?.user_id === req?.session?.user?.id;
   res.render('post', { post, isAuthor });
 });
 
